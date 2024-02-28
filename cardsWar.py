3a82c58aa40f405d71a43e837c2c03a55d5bd474
izabelabarbara <134167905+izabelabarbara@users.noreply.github.com>
2024-02-28 21:40:05 +0100

    Update cardsWar.py

--- cardsWar.py
+++ cardsWar.py
@@ -49,10 +49,57 @@ class Deck:
 
 class Player:
   def __init__(self, name):
-    self.wins = 0
+    self.score = 0
     self.card = None
     self.name = name
 
 
 class Game:
-  
+  def __init__(self):
+    name1 = input("Imię gracza 1: ")
+    name2 = input("Imię gracza 2: ")
+    self.deck = Deck()
+    self.p1 = Player(name1)
+    self.p2 = Player(name2)
+
+  def wins(self, winner):
+    w = "Tę rundę wygrywa {}.".format(winner)
+    print(w)
+
+  def draw(self, p1n, p1c, p2n, p2c):
+    d = "{} wyciągnął {}. {} wyciągnął {}.".format(p1n, p1c, p2n, p2c)
+    print(d)
+
+  def play_game(self):
+    cards = self.deck.cards
+    print("Zaczynamy grę!")
+    while len(cards) >=2:
+      response = input("Naciśnij 'q', jeśli chcesz zakończyć. Naciśnij jakikolwiek inny klawisz, by kontynuować grę.")
+      if response == 'q':
+        break
+      p1c = self.deck.rm_card()
+      p2c = self.deck.rm_card()
+      p1n = self.p1.name
+      p2n = self.p2.name
+      self.draw(p1n, p1c, p2n, p2c)
+      if p1c > p2c:
+        self.p1.score += 1
+        self.wins(self.p1.name)
+      else:
+        self.p2.score += 1
+        self.wins(self.p2.name)
+
+    win = self.winner()
+
+    print("Gra skończona! {}".format(self.winner()))
+
+  def winner(self):
+    if self.p1.score > self.p2.score:
+      return "Wygrywa {}".format(self.p1.name)
+    if self.p1.score < self.p2.score:
+      return "Wygrywa {}".format(self.p2.name)
+    return "It's a draw!"
+
+
+game = Game()
+game.play_game()
